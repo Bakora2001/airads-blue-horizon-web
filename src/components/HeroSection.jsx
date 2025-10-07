93c1ed0c92ad70f862c35585e5e38d92d59ea504
Bakora2001 <maxwellbakora@gmail.com>
2025-10-07 20:56:41 +0300

    Added

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,10 +7,10 @@ const HeroSection = () => {
     'src/public/6.7.JPG',
     'src/public/7.1.JPG',
     'src/public/11.jpg',
-    'src/public/14 (2).jpg',
-    'src/public/17.jpg',
-    'src/public/31.jpg',
-    '../public/33.JPG'
+    '/14 (2).jpg',
+    '/17.jpg',
+    '/31.jpg',
+    '/image33.jpg'
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
